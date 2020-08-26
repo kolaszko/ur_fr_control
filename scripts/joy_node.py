4e8d1139eda95f66f4e835b5eb944b8e41120d82
Mikołaj Łysakowski <mikolaj.lysakowski.bk@gmail.com>
2020-08-26 12:52:25 +0200

    Add orientation control over joy, add deadzone

--- scripts/joy_node.py
+++ scripts/joy_node.py
@@ -9,30 +9,38 @@ import message_filters
 
 import robot_controller
 
+
 class Ur3JoyControllerRos():
-    def __init__(self, ip='150.254.47.146', joy_topic="/joy", scale = 0.005):
+    def __init__(self, ip='150.254.47.171', joy_topic="/joy", scale=0.001):
         self.robot = robot_controller.Ur3(ip, 30003, 30002)
         self.sub = message_filters.Subscriber(joy_topic, Joy)
         self.cache = message_filters.Cache(self.sub, cache_size=1, allow_headerless=False)
         self.scale = scale
+        self.orient_scale = 0.01
         self.last_pose = self.robot.get_pose()
 
     def callback(self):
         data = self.cache.getLast()
         if data is not None:
-            if data.axes[0] == 0 and data.axes[1] == 0 and data.axes[4] == 0:
+            if abs(data.axes[0]) < 0.1 and abs(data.axes[1]) < 0.1 and abs(data.axes[4]) < 0.1:
                 print("zeros")
                 return
             try:
-                dx = data.axes[1] * self.scale
-                dy = data.axes[0] * self.scale
-                dz = data.axes[4] * self.scale
-                self.last_pose =  self.last_pose + np.array([dx, dy, dz, 0, 0, 0])
+                if data.buttons[5] == 0:
+                    dx = data.axes[1] * self.scale
+                    dy = data.axes[0] * self.scale
+                    dz = data.axes[4] * self.scale
+                    self.last_pose = self.last_pose + np.array([dx, dy, dz, 0, 0, 0])
+                elif data.buttons[5] == 1:
+                    dx = data.axes[1] * self.orient_scale
+                    dy = data.axes[0] * self.orient_scale
+                    dz = data.axes[4] * self.orient_scale
+                    self.last_pose = self.last_pose + np.array([0, 0, 0, dx, dy, dz])
+
                 self.robot.move([self.last_pose], False, True, a=1, v=0.05)
 
             except:
                 print("Oops!")
-
 
 
 if __name__ == '__main__':
